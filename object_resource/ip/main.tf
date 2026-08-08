678012bf42c19b640dae7b353de163ac6dfb6ff4
sanjeevPanwar18407 <sanjeevPanwar18407@gmail.com>
2026-08-08 15:47:25 +0530

    new commit

--- object_resource/ip/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_public_ip" "rgs" {
-  for_each = var.ip 
-  name = each.value.ip_name
-  location = each.value.location
-  resource_group_name = each.value.resource_group_name
-  allocation_method = each.value.allocation_method
-}
-
--- object_resource/resource_group/main.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_resource_group" "rgs" {
-  for_each = var.rg
-  name = each.value.name
-  location = each.value.location
-}
-
